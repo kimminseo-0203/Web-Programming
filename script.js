0e92111c81e23e8015c11371d9e02ff724d0df26
kimminseo-0203 <73935779+kimminseo-0203@users.noreply.github.com>
2023-12-10 18:31:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -36,14 +36,41 @@ function showGalleryRange(start, end) {
     }
 }
 
-// 이벤트 리스너 추가
+// 페이지 로딩 시 실행할 초기 코드 추가
 document.addEventListener("DOMContentLoaded", function() {
-    // 페이지 로딩 시 실행할 초기 코드 추가...
-
     // 초기 슬라이드 표시
     var slides = document.getElementsByClassName("slide");
     for (var i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slides[currentSlide].style.display = "block";
+    slides[currentSlide]..style.display = "block";
 });
+
+// 스크롤 애니메이션
+window.onscroll = function() {
+    var sections = document.getElementsByTagName('section');
+    for (var i = 0; i < sections.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = sections[i].getBoundingClientRect().top;
+        var elementVisible = 150; // 픽셀 단위
+
+        if (elementTop < windowHeight - elementVisible) {
+            sections[i].classList.add("active");
+        } else {
+            sections[i].classList.remove("active");
+        }
+    }
+};
+
+// 동적 데이터 로드
+function loadMoreData() {
+    // AJAX 요청 혹은 다른 방식으로 데이터 로드 구현
+    console.log("추가 데이터 로드");
+    // 데이터 처리 및 표시 로직 구현
+}
+
+// 윈도우 리사이즈 이벤트 핸들링
+window.onresize = function(event) {
+    // 리사이즈 시 실행할 코드
+    console.log("윈도우 크기가 변경되었습니다.");
+};
